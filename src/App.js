99e8c626017663834c50082b0bdda5cdc9dd2477
dmunozw <36782576+dmunozw@users.noreply.github.com>
2018-06-24 14:29:23 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -181,7 +181,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('https://lit-atoll-33236.herokuapp.com:3001/image', {
+        fetch('https://lit-atoll-33236.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
